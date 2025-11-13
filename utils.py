9318204e69e4dde600e760d6f81864078e639732
ourpixelworld77-art <ourpixelworld77@gmail.com>
2025-11-13 15:51:01 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,11 @@
+from pytrends.request import TrendReq
+import pandas as pd
+
+def get_custom_trends():
+    pytrends = TrendReq(hl="en-US", tz=0)
+    # Aqui você pode definir uma lista de tópicos específicos
+    kw_list = ["Python", "YouTube", "Gaming"]
+    pytrends.build_payload(kw_list, timeframe='now 7-d')
+    data = pytrends.interest_over_time()
+    return data.fillna(0).to_dict()
 
